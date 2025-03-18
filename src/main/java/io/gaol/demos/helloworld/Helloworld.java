0ac2b48370874a8c8c04401f6a80447972c1996e
Lin Gao <aoingl@gmail.com>
2025-03-18 10:33:07 +0800

    Add another endpoint to trigger new push

--- src/main/java/io/gaol/demos/helloworld/Helloworld.java
+++ src/main/java/io/gaol/demos/helloworld/Helloworld.java
@@ -1,5 +1,7 @@
 package io.gaol.demos.helloworld;
 
+import java.util.Random;
+
 import io.quarkus.vertx.web.Route;
 import io.vertx.ext.web.RoutingContext;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -7,9 +9,16 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class Helloworld {
 
+    private static final Random random = new Random(100);
+
     @Route(path = "/hello", methods = Route.HttpMethod.GET)
     void hello(RoutingContext rc) {
         rc.response().end(System.getenv()
         .getOrDefault("HOSTNAME", "default_hostname"));
+    }
+
+    @Route(path = "/guess", methods = Route.HttpMethod.GET)
+    void guess(RoutingContext rc) {
+        rc.response().end(String.format("Guess the number: %d", random.nextInt(100)));
     }
 }
