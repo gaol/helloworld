05eb3b49f5accb8bfcd5abc3e174455c8d57fdfb
Lin Gao <aoingl@gmail.com>
2025-03-18 10:38:01 +0800

    fix random issue in native

--- src/main/java/io/gaol/demos/helloworld/Helloworld.java
+++ src/main/java/io/gaol/demos/helloworld/Helloworld.java
@@ -1,7 +1,5 @@
 package io.gaol.demos.helloworld;
 
-import java.util.Random;
-
 import io.quarkus.vertx.web.Route;
 import io.vertx.ext.web.RoutingContext;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -9,7 +7,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class Helloworld {
 
-    private static final Random random = new Random(100);
 
     @Route(path = "/hello", methods = Route.HttpMethod.GET)
     void hello(RoutingContext rc) {
@@ -17,8 +14,4 @@ public class Helloworld {
         .getOrDefault("HOSTNAME", "default_hostname"));
     }
 
-    @Route(path = "/guess", methods = Route.HttpMethod.GET)
-    void guess(RoutingContext rc) {
-        rc.response().end(String.format("Guess the number: %d", random.nextInt(100)));
-    }
 }
